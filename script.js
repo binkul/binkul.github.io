2a5820eb28088c4666737d1839aad1b589177f4f
binkul <jacekbinkul@gmail.com>
2020-03-10 21:17:34 +0100

    Original for module

--- script.js
+++ script.js
@@ -111,7 +111,9 @@ $(document).ready(function() {
     var requestUrl = apiRoot + 'deleteTask';
 
     $.ajax({
-      url: requestUrl + '/' + taskId,
+      url: requestUrl + '/?' + $.param({
+        taskId: taskId
+      }),
       method: 'DELETE',
       success: function() {
         parentEl.slideUp(400, function() { parentEl.remove(); });
